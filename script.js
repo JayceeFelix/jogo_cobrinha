688caadc4d295e7942a649353b2d595ea199b4a0
Jaycee <jayceefelix26@gmail.com>
2021-07-18 03:49:55 -0300

    adicionou a comidinha e definiu a interação cobra/comidinha

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ snake[0] = {
 
 let direction = "right";
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -22,10 +27,14 @@ function criarCobrinha() {
     }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener("keydown", update);
 
 function update(event){
-    console.log("update funcao" + event)
     if (event.keyCode == 37 && direction != "right") direction = "left";
     if (event.keyCode == 38 && direction != "down") direction = "up";  
     if (event.keyCode == 39 && direction != "left") direction = "right";
@@ -40,6 +49,7 @@ function inciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -49,7 +59,12 @@ function inciarJogo() {
     if (direction=="up") snakeY -= box;
     if (direction=="down") snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    } else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y= Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
